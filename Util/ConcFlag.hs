59d01d62993199781986bc5692fbd448ea5c0525
atzeus <ploeg@cwi.nl>
2014-10-10 13:02:37 +0200

    small comment update

--- Util/ConcFlag.hs
+++ Util/ConcFlag.hs
@@ -7,7 +7,7 @@ type Flag = MVar ()
 {- A concurrent flag signaling if an update should take place,
    designed for one reader and multiple writers
    if the flag is set, the reader has not seen the flag yet,
-   and resetting the flag is idempotent. -}
+   and setting the flag is idempotent. -}
 
 newFlag :: IO Flag
 newFlag = newEmptyMVar
