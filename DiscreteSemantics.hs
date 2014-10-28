b0ac182dda87561822693c82d0449eeae5929979
atze <ploeg@cwi.nl>
2014-10-28 19:52:47 +0100

    Proof added

--- DiscreteSemantics.hs
+++ DiscreteSemantics.hs
@@ -1,6 +1,11 @@
 import Control.Applicative
 import Control.Monad.Fix
 
+-- these discrete semantics correspond to the actual semantics by
+-- the following function:
+
+-- [.] = toDenotation
+-- [x `Step` (te,y)] = \t -> if t < te then x else [y] t
 
 data Behaviour a = Step { getHead :: a, getTail :: Event (Behaviour a) }
 
@@ -8,10 +13,29 @@ instance Monad Behaviour where
   return x = x `Step` never
   (h `Step` t) >>= f = f h `switch` fmap (>>= f) t
 
--- this might be correct?
+
 instance MonadFix Behaviour where
-  mfix f = let b = loop b in b where
-    loop (a `Step` t) = f a `switch` fmap loop t
+  mfix f = let b = (a `Step` t) = f a in b 
+
+-- That the above corresponds to the 
+-- semantics is not so obvious (to me)
+--
+-- To prove:
+-- [mfix f] = [mfix] (\a -> [f a])
+
+-- Proof by rewriting
+-- lhs : [let b@(a `Step` t) = f a in b]                                  -- unfold of mfix
+--       let b@(a `Step` t) = f a in [b]                                  -- push [.] inwards
+--       let (a `Step` (te,y)) = f a in \t -> if t < te then a else [y] t -- unfold [.]
+--       \t -> let (a `Step` (te,y)) = f a in if t < te then a else [y] t -- lift lambda
+--
+-- rhs : (\f t -> let a = f a t in a) (\a -> [f a]) -- unfold mfix(denot)
+--       \t -> let a = [f a] t in a                  -- inline arg
+--       \t -> let a = let (a `Step` (te,y) in if t < te then x else [y] t) in a in a -- unfold [.]
+--       \t -> let (a `Step` (te,y)) = f a in if t < te then a else [y] t  -- rewrite lets
+
+
+
 
 (h `Step` t) `switch` e = h `Step` (fmap nxt $ race t e) where
   nxt (Left b)  = b `switch` e
