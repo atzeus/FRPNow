35692e53d4fc51cd2e7d1e60f61938599d6e72a8
Atze van der Ploeg <ploeg@cwi.nl>
2015-01-13 13:35:31 +0100

    Moving stuff

--- Semantics/DiscreteSemantics.hs
+++ Semantics/DiscreteSemantics.hs
@@ -12,12 +12,7 @@ data Behaviour a = (:->) { headB :: a, tailB ::  Event (Behaviour a) }
 
 instance Monad Behaviour where
   return x        = x :-> never
-  (h :-> t) >>= f = f h `switch` fmap (>>= f) t
 
-{-
-instance MonadFix Behaviour where
-  mfix f = fix (f . headB)
--}
 
 switch :: Behaviour a -> Event (Behaviour a) -> Behaviour a
 switch (h :-> t) e = h :-> (either (`switch` e) id) <$> (first t e) 
@@ -28,8 +23,4 @@ whenJust (h :-> t) =
   in case h of
    Just x  -> pure x :-> t'
    Nothing -> (t' >>= headB) :-> t'
-
-seqS :: Behaviour x -> Behaviour a -> Behaviour a
-seqS  s@(sh :-> st) b@(h :-> t) = 
-  (sh `seq` h) :-> ((s `seqS`) <$> t) 
 
