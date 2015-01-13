4ba0659a224b52c3b2a09d6ad6b328d62c040733
Atze van der Ploeg <ploeg@cwi.nl>
2015-01-13 13:33:24 +0100

    Remove stuff

--- Control/FRPNowImpl/Behavior.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE RecursiveDo, ScopedTypeVariables, LambdaCase #-}
-module Control.FRPNowImpl.Behavior(Behavior,curIO, switch, whenJust, seqb, seqAlways) where
-import Control.Applicative hiding (empty,Const)
-import Control.Monad
-import Control.Concurrent.MVar
-import System.IO.Unsafe
-import Control.FRPNowImpl.Now
-
-
