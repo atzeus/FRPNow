ec00220ccb0029f04e818de0cafdd32851deeaf9
atze <ploeg@cwi.nl>
2015-01-21 11:24:08 +0100

    Even simpler...

--- Semantics/Simplest.hs
+++ Semantics/Simplest.hs
@@ -1,23 +1,17 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, LambdaCase #-}
 
 import Control.Applicative
 import Control.Monad
 import Data.Maybe
+import Control.Monad.Trans.Maybe
 
-data Event m a = E { runE :: m (Maybe a) }
+newtype Event m a = E (MaybeT m a) deriving Monad
+runE (E m) = runMaybeT m
 
 never :: Monad m => Event m a
-never = E (return Nothing)
+never = E mzero
 
-instance Monad m => Monad (Event m) where
-  return x = E $ return (Just x)
-  m >>= f = E $ runE m >>= maybe (return Nothing) (runE . f)
-
-data Behavior m a = B { runB :: m a }
-
-instance Monad m => Monad (Behavior m) where
-  return x = B $ return x
-  m >>= f  = B $ runB m >>= runB . f
+newtype Behavior m a = B { runB :: m a } deriving Monad
 
 switch :: Monad m=>  Behavior m a -> Event m (Behavior m a) -> Behavior m a
 switch b e = B $ runE e >>= runB . fromMaybe b
@@ -26,5 +20,5 @@ class Monad m => Plan m where
   everyRound :: m (Maybe a) -> m (Event m a)
 
 whenJust :: Plan m => Behavior m (Maybe a) -> Behavior m (Event m a)
-whenJust b = B $ everyRound (runB b) where
+whenJust b = B $ everyRound (runB b)
 
