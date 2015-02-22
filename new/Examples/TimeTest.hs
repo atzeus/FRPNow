762a2e671498e9a7d1f7ff69be0fb831c25d7bd5
atze <ploeg@cwi.nl>
2015-02-22 11:33:43 +0100

    More stuff

--- new/Examples/TimeTest.hs
+++ new/Examples/TimeTest.hs
@@ -1,44 +1,38 @@
-{-# LANGUAGE LambdaCase,NoMonomorphismRestriction,TypeOperators, ViewPatterns, RecursiveDo, ScopedTypeVariables #-}
 
 
-import FRPNow
+
+import Impl.FRPNow
 import Lib.Lib
 
 import Control.Concurrent
 import Control.Applicative
 import Control.Monad hiding (when)
-import Control.Concurrent.MVar 
-import System.IO
+
 main = do runFRP testb   >>= putStrLn . show
 
 
 testb = 
   do b <- count1 500000 :: Now (Behavior Int)
      e <- count1 250000 :: Now (Behavior Int)
-     -- let c = (+) <$> b <*> e
-     showChanges ((,) <$> b <*> e)
-     {-ev <- cur $ when ((1 <=) <$> b)
-     x <- cur $ (bla (10 <$ ev))
-     return  (x :: Event Int)
-     -}
-     return (never :: Event Int)
+     let c = (+) <$> b <*> e
+     showChanges ((,,) <$> c <*> b <*> e)
+     cur $ when ((100 <=) <$> c)
 
 count1 :: Int -> Now (Behavior Int)
 count1 delay = loop 0 where
   loop i = 
-    do 
-       e <- async (threadDelay  delay)
-       e' <- plan (fmap (const (loop (i+1))) e)
+    do e <- async (threadDelay  delay)
+       e' <- plan (loop (i+1) <$ e)
        return (pure i `switch` e')
 
+{-
 bla e1 = let e = when $ (== 1) <$> b
              b = e >>= \e2 ->  pure 0  `switch` (pure 2 <$ e2)
          in b
 
-{-
 bla :: Event Int -> Behavior (Event Int)
 bla e = loop where
-  loop = occ e >>= \case
+  loop = occ e >>= \x -> case x of
            Just a -> pure (pure 1)
            Nothing -> do e' <- join <$> plan (loop <$ e) 
                          pure e' `switch` (loop <$ e')
--- new/Lib/EventStream.hs
+++ new/Lib/EventStream.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE DeriveFunctor,FlexibleInstances,ConstraintKinds,ViewPatterns,NoMonomorphismRestriction,MultiParamTypeClasses ,FlexibleContexts,TypeOperators, LambdaCase, ScopedTypeVariables, Rank2Types, GADTs, TupleSections,GeneralizedNewtypeDeriving, UndecidableInstances #-}
+
 
 module Lib.EventStream
--- (EventStream(..), next, nextSim, emptyEs, repeatEv, merge, switchEs, singletonEs, fmapB, filterJusts, foldB, fold, during, sampleOn, parList, scanlEv, last,filterEv, bufferStream, foldr1Ev, foldrEv, foldrSwitch, changes, EventStreamM, emit,runEventStreamM, printAll, repeatEvN)
+(EventStream, next, nextSim, repeatEvN,repeatEv, merge, fmapB, filterJusts, filterEv, filterMapB, 
+  filterB, during,sampleOn, scanlEv, foldr1Ev, foldrEv, foldrSwitch, foldB, fold, parList, bufferStream, fromChanges, printAll)
   where
 
 import Data.Maybe
@@ -53,20 +54,6 @@ merge l r = loop where
   nxt (Tie  l r) = l ++ r
   nxt (L    l  ) = l 
   nxt (R      r) = r 
-
-tailEs :: EventStream a -> Behavior (Event (EventStream a))
-tailEs es = fmap (const es) <$> getEs es
-               
-switchEs :: EventStream a -> EventStream a -> EventStream a
-switchEs l r = Es $ do r' <- fmap getEs <$> tailEs r
-                       getEs (merge l r) `switch` r'
-
-emptyEs :: EventStream a
-emptyEs = Es $ return never
-
-singletonEs :: Event a -> EventStream a
-singletonEs e = Es $ pure (fmap (\x -> [x]) e) `switch` fmap (const (getEs emptyEs)) e
-
 
 
 fmapB :: Behavior (a -> b) -> EventStream a -> EventStream b
@@ -152,3 +139,79 @@ bufferStream i = scanlEv (\t h -> take i (h : t)) []
 fromChanges :: Eq a => Behavior a -> EventStream a
 fromChanges = repeatEv . changeVal 
 
+printAll :: (Show a, Eq a) => EventStream a -> Now ()
+printAll evs = do e2 <- cur (nextSim evs)
+                  plan (fmap loop e2)
+                  return () where
+  loop l = 
+      do unsafeSyncIO (mapM_ (putStrLn . show) l)
+         e2 <- cur (nextSim evs)
+         plan (loop <$> e2)
+         return ()            
+
+{- 
+-- See reflection without remorse for which performance problem this construction solves...
+
+type Evs     x = Seq (EvsView x)
+type EvsView x = Event (EH x)
+data EH x = x :| Evs x | End
+
+
+toView :: Evs x -> EvsView x
+toView e = case viewl e of
+     EmptyL -> return End
+     h :< t -> h >>= nxt t
+  where nxt t End = toView t
+        nxt r (h :| l) = let q = l >< r in return (h :| q)
+
+append :: Evs x -> Evs x -> Evs x
+append = (><)
+
+app :: Evs x -> Event (Evs x) -> Evs x
+app l r = append l (singleton $ join $ fmap toView r) -- it's magic!
+
+emptyEmits = empty
+singleEmit x = singleton (return (x :| emptyEmits))
+
+toEventStream :: Evs x -> EventStream x
+toEventStream = Es . loop where
+  loop e = do e' <- lose e
+              eh <- join <$> plan (nxt [] <$> toView e')
+              pure eh `switch` (loop e' <$ eh)
+  lose e = getNow (toView e) >>= \case 
+            Just End      -> return emptyEmits
+            Just (h :| t) -> lose t
+            Nothing       -> return e
+  nxt :: [x] -> EH x -> Behavior (Event ([x]))
+  nxt [] End     = return never
+  nxt l  End     = return (return (reverse l))
+  nxt l (h :| t) = getNow (toView t) >>= \case 
+                    Just x -> nxt (h : l) x
+                    Nothing -> return (return (reverse (h: l)))
+
+data EventStreamM x a = EventStreamM { emits :: Evs x, eend :: Event a }
+
+instance Monad (EventStreamM x) where
+  return x = EventStreamM emptyEmits (return x)
+  (EventStreamM s e) >>= f = let fv = fmap f e
+                                 fs = emits <$> fv
+                                 fa = fv >>= eend
+                             in EventStreamM (app s fs) fa
+
+emit :: (Swap (BehaviorEnd x) f, Monad f) =>  x -> (f :. EventStreamM x) ()
+emit x = liftRight $ EventStreamM (singleEmit x) (return ())
+
+instance Wait (EventStreamM x) where 
+  waitEv = EventStreamM emptyEmits
+
+instance (Monad b, Swap Event b) => Swap (EventStreamM x) b where
+  swap (EventStreamM b e) = liftM (EventStreamM b) (plan e)
+
+instance Functor (EventStreamM x) where fmap = liftM
+instance Applicative (EventStreamM x) where pure = return ; (<*>) = ap
+
+runEventStreamM :: EventStreamM x a -> (EventStream x, Event a)
+runEventStreamM (EventStreamM s e) = (toEventStream s, e)
+  
+
+-}
