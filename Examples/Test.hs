05f0c5c2e562a7b9cac61849c9bf48d3ea92815d
Atze van der Ploeg <atzeus@gmail.com>
2015-06-26 17:02:12 +0200

    More moving stuff around

--- Examples/Test.hs
+++ Examples/Test.hs
@@ -1,18 +1,16 @@
-import FRPNow 
-import NowMaster
-import Lib
+import Control.FRPNow 
 import Control.Applicative
 
 n = 11000
 
 main = runNowMaster (test n)
 
-test :: Int -> Now (E ())
+test :: Int -> Now (Event ())
 test n = do b <- count 
             e <- sample (when ((n ==) <$> b))
             return e
 
-count :: Now (B Int)
+count :: Now (Behavior Int)
 count = loop 0 where
   loop i =  do  e <- async (return ())
                 e'<- planNow (loop (i+1) <$ e)
